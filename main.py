9d5474e647a7e5299e07709d6f37439683fc2f51
Sevdaakari <sevda.akari@gmail.com>
2024-02-03 00:03:39 +0100

    fixed

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for, request
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+Bootstrap5(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=False, port=5006)
